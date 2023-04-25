aad5255ab87fbe99dc6d2c6e5852fbe77477d8ec
Ethan1709 <benyayerethan@gmail.com>
2023-04-25 08:50:16 +0200

    adding task 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,22 +1,58 @@
 $(document).ready(function() {
-  $.ajax({
-    type: "POST",
-    url: "http://0.0.0.0:5001/api/v1/places_search",
-    data: JSON.stringify({}),
-    contentType: "application/json; charset=UTF-8",
-    dataType: "json",
-    success: function(data) { 
-      console.log('OK');
-      console.log(data);
-      // レスポンス(data)からplaceをループ処理
-      for (let i = 0; i < data.length; i++) {
-        const place = data[i];
-        console.log(place);
+  let amenities = {};
 
-        let article = $('<article>');
-        $('article').append('<div>');
-        $('div').append('<h2>' + place.name + '</h2>');
-      }
-      }
+  $('input[type="checkbox"]').change(function() {
+    if ($(this).prop('checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    let text = '';
+    for (let amenity in amenities) {
+      text += amenities[amenity] + ', ';
+    }
+    text = text.slice(0, -2); // remove last comma and space
+    $('div.amenities h4').text(text);
+  });
 });
+
+$(document).ready(function() {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function(data, status) {
+      if (data.status === 'OK') {
+          $('#api_status').addClass('available');
+      } else {
+          $('#api_status').removeClass('available');
+      }
+  });
+});
+
+$(document).ready(function () {
+  const url = 'http://0.0.0.0:5001/api/v1/places_search/';
+  const data = {};
+  const options = {
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify(data),
+  };
+  $.ajax(url, options).done(function (places) {
+    $.each(places, function (index, place) {
+      const article = [
+        '<article>',
+        '<div class="title_box">',
+        '<h2>' + place.name + '</h2>',
+        '<div class="price_by_night">$' + place.price_by_night + '</div>',
+        '</div>',
+        '<div class="information">',
+        '<div class="max_guest">' + place.max_guest + ' Guest' + (place.max_guest !== 1 ? 's' : '') + '</div>',
+        '<div class="number_rooms">' + place.number_rooms + ' Bedroom' + (place.number_rooms !== 1 ? 's' : '') + '</div>',
+        '<div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom' + (place.number_bathrooms !== 1 ? 's' : '') + '</div>',
+        '</div>',
+        '<div class="description">',
+        place.description,
+        '</div>',
+        '</article>',
+      ].join('\n');
+      $('section.places').append(article);
+    });
+  });
 });
